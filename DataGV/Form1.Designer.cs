1231078f64567879e71d137fcdc4c64bcfe0f7d9
Juan Proyectos <jngproyectos@hotmail.com>
2022-06-05 09:14:26 -0500

    Agregar archivos de proyecto.

---
+++ DataGV/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace DataGV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pTop = new System.Windows.Forms.Panel();
+            this.pSecond = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnM = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.pBtns = new System.Windows.Forms.Panel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.dgvEjemplo = new System.Windows.Forms.DataGridView();
+            this.pTop.SuspendLayout();
+            this.pSecond.SuspendLayout();
+            this.pBtns.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEjemplo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pTop
+            // 
+            this.pTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.pTop.Controls.Add(this.pBtns);
+            this.pTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pTop.Location = new System.Drawing.Point(0, 0);
+            this.pTop.Name = "pTop";
+            this.pTop.Size = new System.Drawing.Size(550, 40);
+            this.pTop.TabIndex = 0;
+            // 
+            // pSecond
+            // 
+            this.pSecond.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.pSecond.Controls.Add(this.lblTitulo);
+            this.pSecond.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pSecond.Location = new System.Drawing.Point(0, 40);
+            this.pSecond.Name = "pSecond";
+            this.pSecond.Size = new System.Drawing.Size(550, 50);
+            this.pSecond.TabIndex = 1;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Lato", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTitulo.Location = new System.Drawing.Point(30, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(390, 33);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "JnG -Proyectos - DataGridView";
+            // 
+            // btnMin
+            // 
+            this.btnMin.FlatAppearance.BorderSize = 0;
+            this.btnMin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
+            this.btnMin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(165)))), ((int)(((byte)(166)))));
+            this.btnMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMin.Image = global::DataGV.Properties.Resources.min;
+            this.btnMin.Location = new System.Drawing.Point(1, 1);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(50, 35);
+            this.btnMin.TabIndex = 2;
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnM
+            // 
+            this.btnM.FlatAppearance.BorderSize = 0;
+            this.btnM.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
+            this.btnM.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(165)))), ((int)(((byte)(166)))));
+            this.btnM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnM.Image = global::DataGV.Properties.Resources.max;
+            this.btnM.Location = new System.Drawing.Point(51, 1);
+            this.btnM.Name = "btnM";
+            this.btnM.Size = new System.Drawing.Size(50, 35);
+            this.btnM.TabIndex = 2;
+            this.btnM.UseVisualStyleBackColor = true;
+            this.btnM.Click += new System.EventHandler(this.btnM_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.FlatAppearance.BorderSize = 0;
+            this.btnC.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(101)))), ((int)(((byte)(113)))));
+            this.btnC.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(3)))));
+            this.btnC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnC.Image = global::DataGV.Properties.Resources.close;
+            this.btnC.Location = new System.Drawing.Point(101, 1);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(50, 35);
+            this.btnC.TabIndex = 2;
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // pBtns
+            // 
+            this.pBtns.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pBtns.Controls.Add(this.btnC);
+            this.pBtns.Controls.Add(this.btnMin);
+            this.pBtns.Controls.Add(this.btnM);
+            this.pBtns.Location = new System.Drawing.Point(396, 1);
+            this.pBtns.Name = "pBtns";
+            this.pBtns.Size = new System.Drawing.Size(154, 39);
+            this.pBtns.TabIndex = 3;
+            // 
+            // btn1
+            // 
+            this.btn1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(129)))), ((int)(((byte)(189)))));
+            this.btn1.FlatAppearance.BorderSize = 0;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn1.Location = new System.Drawing.Point(12, 96);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(70, 34);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn2.BackColor = System.Drawing.Color.Black;
+            this.btn2.FlatAppearance.BorderSize = 0;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn2.Location = new System.Drawing.Point(88, 96);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(70, 34);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(80)))), ((int)(((byte)(77)))));
+            this.btn3.FlatAppearance.BorderSize = 0;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn3.Location = new System.Drawing.Point(164, 96);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(70, 34);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(187)))), ((int)(((byte)(89)))));
+            this.btn4.FlatAppearance.BorderSize = 0;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn4.Location = new System.Drawing.Point(240, 96);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(70, 34);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(100)))), ((int)(((byte)(162)))));
+            this.btn5.FlatAppearance.BorderSize = 0;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn5.Location = new System.Drawing.Point(316, 96);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(70, 34);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(172)))), ((int)(((byte)(198)))));
+            this.btn6.FlatAppearance.BorderSize = 0;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn6.Location = new System.Drawing.Point(392, 96);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(70, 34);
+            this.btn6.TabIndex = 8;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(150)))), ((int)(((byte)(70)))));
+            this.btn7.FlatAppearance.BorderSize = 0;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn7.Location = new System.Drawing.Point(468, 96);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(70, 34);
+            this.btn7.TabIndex = 9;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // dgvEjemplo
+            // 
+            this.dgvEjemplo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvEjemplo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEjemplo.Location = new System.Drawing.Point(12, 136);
+            this.dgvEjemplo.Name = "dgvEjemplo";
+            this.dgvEjemplo.Size = new System.Drawing.Size(526, 313);
+            this.dgvEjemplo.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.ClientSize = new System.Drawing.Size(550, 500);
+            this.Controls.Add(this.dgvEjemplo);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pSecond);
+            this.Controls.Add(this.pTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pTop.ResumeLayout(false);
+            this.pSecond.ResumeLayout(false);
+            this.pSecond.PerformLayout();
+            this.pBtns.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEjemplo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pTop;
+        private System.Windows.Forms.Panel pSecond;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnM;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Panel pBtns;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.DataGridView dgvEjemplo;
+    }
+}
+
---
+++ DataGV/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnM_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+                WindowState = FormWindowState.Maximized;
+            else if (WindowState == FormWindowState.Maximized)
+                WindowState = FormWindowState.Normal;
+        }
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+                WindowState = FormWindowState.Minimized;
+            else if (WindowState == FormWindowState.Maximized)
+                WindowState = FormWindowState.Minimized;
+        }
+        #endregion
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximumSize = SystemInformation.PrimaryMonitorMaximizedWindowSize;
+            Data();
+        }
+        //Método para agregar filas y columnas al DataGridView
+        private void Data()
+        {
+            DataGridViewTextBoxColumn cl1 = new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Id",
+                ReadOnly = true
+            };
+            DataGridViewTextBoxColumn cl2 = new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Nombre",
+                ReadOnly = true
+            };
+            DataGridViewTextBoxColumn cl3 = new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Estado",
+                ReadOnly = true
+            };
+            dgvEjemplo.Columns.AddRange(cl1, cl2, cl3);
+            string nombre = "Juan", estado = "Nuevo León";
+            for (int i = 1; i < 30; i++)
+            {
+                dgvEjemplo.Rows.Add(i.ToString(), nombre, estado);
+            }
+        }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            DGVDisenio.Formato(dgvEjemplo, 2);
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            DGVDisenio.Formato(dgvEjemplo, 1);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            DGVDisenio.Formato(dgvEjemplo, 3);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            DGVDisenio.Formato(dgvEjemplo, 4);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            DGVDisenio.Formato(dgvEjemplo, 5);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            DGVDisenio.Formato(dgvEjemplo, 6);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            DGVDisenio.Formato(dgvEjemplo, 7);
+        }
+    }
+}
